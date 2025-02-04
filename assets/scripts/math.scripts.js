9385a240eb62507bb3beb81930599924c6d27658
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-02-04 11:40:51 -0300

    feat: criação de boões com funções

--- assets/scripts/math.scripts.js
+++ assets/scripts/math.scripts.js
@@ -1,11 +1,28 @@
-function calcular() {
+function soma() {
     let numA = Number(document.getElementById("numA").value);
     let numB = Number(document.getElementById("numB").value);
-    
-    // alert(numA);
-    // alert(numB);
 
-    // soma
     let result = numA + numB;
+    document.getElementById("result").innerText = result;
+}
+function sub() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA - numB;
+    document.getElementById("result").innerText = result;
+}
+function mult() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA * numB;
+    document.getElementById("result").innerText = result;
+}
+function divi() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA / numB;
     document.getElementById("result").innerText = result;
 }
